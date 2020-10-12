c329338c7f32643bb83a1cc5368f2e6c2f57ce93
eureka <1920836569@qq.com>
2020-10-12 21:48:38 +0800

    dsahw1_2

--- interview.cpp
+++ interview.cpp
@@ -1,18 +1,18 @@
-#include
-<iostream>
+
+#include<iostream>
 using namespace std;
 
-#typedef int Rank;
+
 #define ListNodePosi(T) ListNode<T>*
-template <typename T> struct ListNode{
+template <typename T> struct ListNode {
     T data; ListNodePosi(T) pred; ListNodePosi(T) succ;
     ListNode() {}
-    ListNode( T e, ListNodePosi(T) p = NULL, ListNodePosi(T) s = NULL)
+    ListNode(T e, ListNodePosi(T) p = NULL, ListNodePosi(T) s = NULL)
         :data(e), pred(p), succ(s) {}
 
     ListNodePosi(T) insertAsPred(T const& e);
     ListNodePosi(T) insertAsSucc(T const& e);
-}
+};
 template <typename T> ListNodePosi(T) ListNode<T>::insertAsPred(T const& e)
 {
     ListNodePosi(T) x = new ListNode(e, pred, this);
@@ -26,22 +26,20 @@ template <typename T> ListNodePosi(T) ListNode<T>::insertAsSucc(T const& e)
     return x;
 }
 
-template <typename T> class <T> List
+template <typename T> class List
 {
 private:
     int _size; ListNodePosi(T) header; ListNodePosi(T) trailer;
 protected:
     void init();
 public:
-    List() {init();}
-    List( List<T> const& L);
+    List() { init(); }
     ~List();
-    ListNodePosi(T) first() const {return header->succ;}
-    ListNodePosi(T) last() const {return trailer->pred;}
-    int insertA(<T> const& e);
-    remove(ListNodePosi<T> p);
-    search(Rank e);
-}
+    ListNodePosi(T) first() const { return header->succ; }
+    ListNodePosi(T) last() const { return trailer->pred; }
+    ListNodePosi(T) insertA(ListNodePosi(T) p, T const& e);
+    T remove(ListNodePosi(T) &p);
+};
 
 template <typename T>
 void List<T>::init()//初始化
@@ -59,13 +57,7 @@ template <typename T>List<T>::~List()//析构
     delete header;
     delete trailer;
 }
-template <typename T>
-T& List<T>::operator[] (Rank r) const//重载[]运算符
-{
-    ListNodePosi(T) p = first();
-    while (0 < r--) p = p->succ;
-    return p->data;
-}
+
 template <typename T>
 ListNodePosi(T) List<T>::insertA (ListNodePosi(T) p, T const& e)//后插入
 {
@@ -73,40 +65,47 @@ ListNodePosi(T) List<T>::insertA (ListNodePosi(T) p, T const& e)//后插入
 }
 
 template <typename T> 
-T List<T>::remove(ListNodePosi(T) p)//删除
+T List<T>::remove(ListNodePosi(T) &p)//删除
 {
     T e = p->data;
+    ListNodePosi(T) temp = p;
     p->pred->succ = p->succ; p->succ->pred = p->pred;
-    delete p; _size--;
+    p = p->pred;
+    delete temp; _size--;
     return e;
 }
 
 int main()
 {
-    sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int n, m, ID;
-    int temp,insertPosi = 0;
+    int temp = -2, insertPosi = -1;
     cin >> n >> m;
     List<int> L;
-    L.first() = new ListNode<int>;
-    pos = L.first();
-    cin >> pos->data;
-    for (i = 1; i < n; i++)
+    ListNodePosi(int) pos = L.last();
+    //cin >> ID;
+    //pos = L.insertA(pos, ID);
+    for (int i = 0; i < n; i++)
     {
+        if(temp < insertPosi - 1)
+            for(int j = 0; j < insertPosi - 1 - temp; j++)
+                pos = pos->succ;
+        else if(temp >= insertPosi)
+            for(int j = 0; j < temp - insertPosi + 1; j++)
+                pos = pos->pred;  
         cin >> ID;
         pos = L.insertA(pos, ID);
-        temp =(insertPosi + m) % i;
-        while(temp--)
-        {
-            if(i > temp)
-                pos = pos->pred;
-            else
-                pos = pos->succ;
-        }
+        temp = insertPosi;
+        insertPosi = (temp + m) % (i + 1);
     }
-    while(n--)
-        cout << L.remove(pos);
+    while(n--){
+        if (pos->pred == NULL)
+            pos = L.last();
+        cout << L.remove(pos) << ' ';
+
+    }
+        
     return 0;
 }
